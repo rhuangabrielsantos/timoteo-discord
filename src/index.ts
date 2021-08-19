c80a077c4625146b02288bcb5b27428768d6301d
Rhuan Gabriel Santos <rhuan.santos@db1.com.br>
2021-08-19 13:28:10 -0300

    :sparkles: Added bot status

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,19 @@ import express from "express";
 import "dotenv/config";
 import Client from "./Client";
 
-new Client({ intents: 32767 }).init();
+new Client({
+  intents: 32767,
+  presence: {
+    status: "online",
+    afk: false,
+    activities: [
+      {
+        name: `${process.env.BOT_PREFIX}help`,
+        type: "LISTENING",
+      },
+    ],
+  },
+}).init();
 
 const server = express();
 const PORT = process.env.PORT || 3000;
