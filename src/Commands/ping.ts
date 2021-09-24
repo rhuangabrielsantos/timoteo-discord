0dff871cd7a733450d08247fdc97205e93db8d90
Rhuan Gabriel Santos <rhuan.santos@db1.com.br>
2021-09-24 13:25:50 -0300

    :memo: Update readme

--- src/Commands/ping.ts
+++ src/Commands/ping.ts
@@ -4,7 +4,7 @@ import { Command } from "../Interfaces";
 
 export const command: Command = {
   name: "ping",
-  description: "Verifica o tempo de resposta do bot.",
+  description: "Verifica o tempo de resposta do servidor.",
   options: [],
   run: async (client: Client) => {
     return `**Pong!** \nO servidor respondeu em ${client.ws.ping}ms!`;
