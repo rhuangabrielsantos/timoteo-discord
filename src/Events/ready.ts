dc2d0b199bc093f0590fdc716270bbdd1c4601be
Rhuan Gabriel Santos <rhuan.santos@db1.com.br>
2021-09-25 11:36:21 -0300

    :sparkles: Commands are updated in ready event

--- src/Events/ready.ts
+++ src/Events/ready.ts
@@ -1,8 +1,29 @@
+import { readdirSync } from "fs";
+import path from "path";
 import { Event } from "../Interfaces";
 
 export const event: Event = {
   name: "ready",
-  run: client => {
+  run: async client => {
     console.log(`${client?.user?.tag} is online!`);
+
+    client.guilds.cache.forEach(async guild => {
+      const commandsToDeploy = [];
+
+      const commandPath = path.join(__dirname, "..", "Commands");
+      readdirSync(commandPath).forEach(async file => {
+        const { command } = require(`${commandPath}/${file}`);
+
+        const commandFormatted = {
+          name: command.name,
+          description: command.description,
+          options: command.options,
+        };
+
+        commandsToDeploy.push(commandFormatted);
+      });
+
+      await guild.commands.set(commandsToDeploy);
+    });
   },
 };
